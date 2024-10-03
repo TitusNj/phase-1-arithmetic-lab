6e1557f95b04f6b5c74b82507b2044de07126b96
Titusnj <titusnj99@gmail.com>
2024-10-03 21:09:03 +0300

    arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
-// Write your code here
+let num1 = 2
+let num2 = 31
+let num3 = 16
+let num4 = 6
+const multiply= num1 * num2;
+//     return num1 * num2
+// }
+// console.log(multiply())
+
+const random = Math.floor(Math.random()*10)+1
+
+
+const mod = num3 % num4; 
+// function mod(){
+//     let num3 = 8
+//     let num4 = 2
+//     return  num3/num4
+// }
+// console.log(mod())
+
+const max = Math.max(1,20,19,4,2);
